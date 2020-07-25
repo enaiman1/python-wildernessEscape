87e82396d0b19604a40d6cabe158cfece95287c5
Ericcentrik <enaiman1@gmail.com>
2020-07-25 16:47:03 -0400

    created a method to transverse the nodes to let the user pick what to do next

--- script.py
+++ script.py
@@ -10,7 +10,20 @@ class TreeNode:
   def add_child(self, node):
     self.choices.append(node)
 
-  
+  def traverse(self):
+    story_node = self
+    print(story_node.story_piece)
+    while len(story_node.choices) > 0:
+      choice = input("Enter 1 or 2 to continue the story: ")
+      if choice not in ["1", "2"]:
+        print("Invalid choice. Try again.")
+      else:
+        chosen_index = int(choice)
+        chosen_index -= 1
+        chosen_child = story_node.choices[chosen_index]
+        print(chosen_child.story_piece)
+        story_node = chosen_child
+
 
 ######
 # VARIABLES FOR TREE
@@ -30,7 +43,7 @@ Do you:
 2 ) Yell 'Hooray!'
 """)
 
-choice_B = TreeNode("""
+choice_b = TreeNode("""
 You come across a clearing full of flowers. 
 The bear follows you and asks 'what gives?'
 Do you:
@@ -40,10 +53,13 @@ Do you:
 
 story_root.add_child(choice_a)
 story_root.add_child(choice_b)
-user_choice = input("What is your name? ")
+
 
 ######
 # TESTING AREA
 ######
 # print(story_root.story_piece)
+# user_choice = input("What is your name? ")
 # print(user_choice)
+story_root.traverse()
+
