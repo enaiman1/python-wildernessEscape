7b78209d7eb271f055ade492b6b0fb10b8aaef5d
Ericcentrik <enaiman1@gmail.com>
2020-07-25 16:36:31 -0400

    added user input and starting creating chapters:

--- script.py
+++ script.py
@@ -7,6 +7,14 @@ class TreeNode:
     self.story_piece = story_piece
     self.choices = []
 
+  def add_child(self, node):
+    self.choices.append(node)
+
+  
+
+######
+# VARIABLES FOR TREE
+######
 story_root = TreeNode("""
 You are in a forest clearing. There is a path to the left.
 A bear emerges from the trees and roars!
@@ -15,12 +23,27 @@ Do you:
 2 ) Run to the left...
 """)
 
-print(story_root.story_piece)
+choice_a = TreeNode("""
+The bear is startled and runs away.
+Do you:
+1 ) Shout 'Sorry bear!'
+2 ) Yell 'Hooray!'
+""")
 
-######
-# VARIABLES FOR TREE
-######
+choice_B = TreeNode("""
+You come across a clearing full of flowers. 
+The bear follows you and asks 'what gives?'
+Do you:
+1 ) Gasp 'A talking bear!'
+2 ) Explain that the bear scared you.
+""")
+
+story_root.add_child(choice_a)
+story_root.add_child(choice_b)
+user_choice = input("What is your name? ")
 
 ######
 # TESTING AREA
 ######
+# print(story_root.story_piece)
+# print(user_choice)
