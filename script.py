87238601da17391e56d2ab525a25dbbac1d3cc5b
Ericcentrik <enaiman1@gmail.com>
2020-07-25 16:22:50 -0400

    created TreeNode class

--- script.py
+++ script.py
@@ -2,6 +2,20 @@ print("Once up a time...")
 ######
 # TREENODE CLASS
 ######
+class TreeNode:
+  def __init__(self, story_piece):
+    self.story_piece = story_piece
+    self.choices = []
+
+story_root = TreeNode("""
+You are in a forest clearing. There is a path to the left.
+A bear emerges from the trees and roars!
+Do you: 
+1 ) Roar back!
+2 ) Run to the left...
+""")
+
+print(story_root.story_piece)
 
 ######
 # VARIABLES FOR TREE
